cf2e65e3b3fe34afe8ab5c74a6be03ebfc94bcc9
Timo Vink <timovink@gmail.com>
2022-06-04 19:26:05 -0700

    Add hosted zones for all owned domain names.

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,25 @@ provider "aws" {
   }
 }
 
+
+locals {
+  primary_domain_name = "timovink.dev"
+  secondary_domain_names = [
+    "timovink.com",
+    "timovink.ca",
+    "timovink.app"
+  ]
+
+  all_domain_names = concat([local.primary_domain_name], local.secondary_domain_names)
+}
+
+
 module "iam" {
   source = "./modules/iam"
+}
+
+module "dns" {
+  source = "./modules/dns"
+
+  domain_names = local.all_domain_names
 }
