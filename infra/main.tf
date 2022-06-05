a04ee0c4ef834d9d8b923ca95fc9a48d1216c931
Timo Vink <timovink@gmail.com>
2022-06-04 20:24:18 -0700

    Create ACM certificates.

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,16 @@ provider "aws" {
   }
 }
 
+provider "aws" {
+  alias  = "global"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project = "tv-sharedinfra"
+    }
+  }
+}
 
 locals {
   primary_domain_name = "timovink.dev"
@@ -20,7 +30,6 @@ locals {
   all_domain_names = concat([local.primary_domain_name], local.secondary_domain_names)
 }
 
-
 module "iam" {
   source = "./modules/iam"
 }
@@ -30,4 +39,21 @@ module "dns" {
 
   for_each    = toset(local.all_domain_names)
   domain_name = each.key
+}
+
+module "local_certificate" {
+  source = "./modules/certificate"
+
+  for_each       = toset(local.all_domain_names)
+  domain_name    = each.key
+  hosted_zone_id = module.dns[each.key].hosted_zone_id
+}
+
+module "global_certificate" {
+  source    = "./modules/certificate"
+  providers = { aws = aws.global }
+
+  for_each       = toset(local.all_domain_names)
+  domain_name    = each.key
+  hosted_zone_id = module.dns[each.key].hosted_zone_id
 }
