9dc96ffc11c6217e209cf0457b5aa4434dde44e8
Timo Vink <timovink@gmail.com>
2022-06-04 19:11:04 -0700

    Add a user with admin access for local use.

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,9 +1,9 @@
-resource "aws_iam_group" "deployer" {
-  name = "deployer"
+resource "aws_iam_group" "administrators" {
+  name = "administrators"
 }
 
-resource "aws_iam_group_policy_attachment" "deployer" {
-  group      = aws_iam_group.deployer.name
+resource "aws_iam_group_policy_attachment" "administrators" {
+  group      = aws_iam_group.administrators.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
@@ -13,5 +13,14 @@ resource "aws_iam_user" "deployer" {
 
 resource "aws_iam_user_group_membership" "deployer" {
   user   = aws_iam_user.deployer.name
-  groups = [aws_iam_group.deployer.name]
+  groups = [aws_iam_group.administrators.name]
+}
+
+resource "aws_iam_user" "tvink" {
+  name = "tvink"
+}
+
+resource "aws_iam_user_group_membership" "tvink" {
+  user   = aws_iam_user.tvink.name
+  groups = [aws_iam_group.administrators.name]
 }
